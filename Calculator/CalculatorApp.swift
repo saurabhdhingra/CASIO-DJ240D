769fab98881a27ff9fe662474f1404c30b0db5c4
saurabhdhingra <saurabh20work@gmail.com>
2024-09-15 21:54:13 +0530

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Saurabh Dhingra on 15/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
