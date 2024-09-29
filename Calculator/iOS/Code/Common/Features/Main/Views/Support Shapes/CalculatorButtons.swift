46b0bc3a76d521553b992f72a66b8b54067d62bd
saurabhdhingra <saurabh20work@gmail.com>
2024-09-30 00:43:29 +0530

    Corrections and Completion

---
+++ Calculator/iOS/Code/Common/Features/Main/Views/Support Shapes/CalculatorButtons.swift
@@ -0,0 +1,18 @@
+//
+//  CalculatorButtons.swift
+//  Calculator
+//
+//  Created by Saurabh Dhingra on 28/09/24.
+//
+
+import SwiftUI
+
+struct CalculatorButtons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalculatorButtons()
+}
